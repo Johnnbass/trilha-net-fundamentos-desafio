fe7efdbc1bbc1eac858221e60073ab9f2584cee2
Jonatha Silveira <jonatha.psilveira@gmail.com>
2023-10-14 00:16:11 -0300

    Complemento do Desafio de Fundamentos

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,4 +1,5 @@
-﻿using DesafioFundamentos.Models;
+﻿using System;
+using DesafioFundamentos.Models;
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -6,8 +7,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\nDigite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
